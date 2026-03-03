556a87437617af0089fc5b0cd0acbcc1784bac80
Syed Muhammad Ahmad Kaka Khel <syedmuhammadahmad939@gmail.com>
2026-03-03 13:53:53 -0800

    Create 02_Variables.java

---
+++ 01_Basics/02_Variables.java
@@ -0,0 +1,26 @@
+public class variables {
+    public static void main(String[] args) {
+        int age ;
+        age =21;
+    System.out.println("age is :"+age);
+        int year;
+        year = 2026;
+    
+    System.out.println("year is :"+year);
+
+    String Name="Syed" ;
+
+    System.out.println("Name is: "+Name);
+    System.out.println("*********************************************");
+    String name="Bro code";
+    int GTA =6;
+    double pi =3.14159;
+    char gender ='M';
+    boolean isAdmin=true;
+
+    System.out.println("name :"+name + "\nGTA:" + GTA+ "\nPI:" + pi +"\ngender:"+gender +"\nisAdmin :"+isAdmin);
+    
+    
+    }
+    
+}
