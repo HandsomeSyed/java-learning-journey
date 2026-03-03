3e8e03f1e112c98c7aae567a5996d971f4ea9556
Syed Muhammad Ahmad Kaka Khel <syedmuhammadahmad939@gmail.com>
2026-03-03 13:52:24 -0800

    Create 01_HelloWorld.java

---
+++ 01_Basics/01_HelloWorld.java
@@ -0,0 +1,7 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("My name is Syed ");
+        System.out.println("I am a 21 year old ");
+    }
+}
